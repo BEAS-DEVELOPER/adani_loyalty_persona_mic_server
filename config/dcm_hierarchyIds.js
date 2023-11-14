943c60c777bcf8d49351e4d3557c95be5b7b89ef
BEAS-DEVELOPER <129931219+BEAS-DEVELOPER@users.noreply.github.com>
2023-11-14 18:01:13 +0530

    A

--- config/dcm_hierarchyIds.js
+++ config/dcm_hierarchyIds.js
@@ -1,13 +1,11 @@
 
 const hirarchyIds =[
 
-    {  id : 1  , name : 'Contractor' , selfReg : true  ,   canRegBy   : [ { id:2, name:'TSO' }, { id:3, name:'Dealer' }]  },
-    {  id : 2  , name : 'TSO'        , selfReg : false ,   canRegBy   : [ { id:2, name:'TSO' }, { id:3, name:'Dealer' }]  },
-    {  id : 3  , name : 'Dealer'     , selfReg : false ,   canRegBy   : [ { id:2, name:'TSO' }, { id:3, name:'Dealer' }]  },
-    {  id : 4  , name : 'Interior'   , selfReg : true  ,   canRegBy   : [ { id:2, name:'TSO' }, { id:3, name:'Dealer' }]  },
-    {  id : 5  , name : 'Engineer'   , selfReg : true  ,   canRegBy   : [ { id:2, name:'TSO' }, { id:3, name:'Dealer' }]  },
-    {  id : 6  , name : 'Architect'  , selfReg : true  ,   canRegBy   : [ { id:2, name:'TSO' }, { id:3, name:'Dealer' }]  },
-    {  id : 7  , name : 'Designers'  , selfReg : true  ,   canRegBy   : [ { id:2, name:'TSO' }, { id:3, name:'Dealer' }]  },
+    {  id : 1  , name : 'Contractor' , selfReg : true  ,   canRegBy   : [ { id:1, name:'TSO' }, { id:2, name:'Dealer' }]  },
+    {  id : 4  , name : 'Interior'   , selfReg : true  ,   canRegBy   : [ { id:1, name:'TSO' }, { id:2, name:'Dealer' }]  },
+    {  id : 5  , name : 'Engineer'   , selfReg : true  ,   canRegBy   : [ { id:1, name:'TSO' }, { id:2, name:'Dealer' }]  },
+    {  id : 6  , name : 'Architect'  , selfReg : true  ,   canRegBy   : [ { id:1, name:'TSO' }, { id:2, name:'Dealer' }]  },
+    {  id : 7  , name : 'Designers'  , selfReg : true  ,   canRegBy   : [ { id:1, name:'TSO' }, { id:2, name:'Dealer' }]  },
 
 ]
 
